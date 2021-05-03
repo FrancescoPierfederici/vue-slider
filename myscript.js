4517c1254c5cf596dd1b071f11ca3d208d25aafd
FrancescoPierfederici <francescopierf@gmail.com>
2021-05-04 00:15:46 +0200

    commit

--- myscript.js
+++ myscript.js
@@ -4,48 +4,63 @@
 // Bonus: Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
 
 
-const app = new Vue ({
-    el:`#vueContainer`,
-    data:{
-        indexImg:0,
-        images: ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg","img/img4.jpg" ],
-        classiArrow:"flex flex-column-content",
-        classiImgContainer:"flex",
-        classiContainer:"justifyContent margin-top",
-        dot:"dot",
-        blue:"color-blue",
-       
+const app = new Vue({
+    el: `#vueContainer`,
+    data: {
+        indexImg: 0,
+        images: ["https://images.pexels.com/photos/4464820/pexels-photo-4464820.jpeg?cs=srgb&dl=pexels-karolina-grabowska-4464820.jpg&fm=jpg","https://images.pexels.com/photos/4464821/pexels-photo-4464821.jpeg?cs=srgb&dl=pexels-karolina-grabowska-4464821.jpg&fm=jpg", "https://images.pexels.com/photos/4464819/pexels-photo-4464819.jpeg?cs=srgb&dl=pexels-karolina-grabowska-4464819.jpg&fm=jpg","https://images.pexels.com/photos/4464815/pexels-photo-4464815.jpeg?cs=srgb&dl=pexels-karolina-grabowska-4464815.jpg&fm=jpg"],
+        classiArrow: "flex flex-column-content",
+        classiImgContainer: "flex",
+        classiContainer: "justifyContent margin-top",
+        dot: "dot",
+        blue: "color-blue",
+
 
     },
-    methods:{
-        fotoSuccessiva: function(){
-            this.indexImg += 1;
-            if(this.indexImg == this.images.length){
-                this.indexImg = 0;
+    methods: {
+        /**
+              * @param {number} direction - +1 se vogliamo andare avanti, -1 se indietro.
+              */
+        changeImg(direction, changedByAI) {
+            let newIndex = this.indexImg + direction;
+
+            if (newIndex < 0) {
+                /*
+                Se indice < di 0, facciamo andare l'utente all'ultima immagine della lista
+                */
+                newIndex = this.images.length - 1;
+
+            } else if (newIndex > (this.images.length - 1)) {
+                /*
+                Se l'indice è già l'ultimo della lista delle immagini, facciamo andare l'utente alla prima immagine
+                */
+                newIndex = 0;
             }
+
+            this.indexImg = newIndex;
         },
-        fotoPrecedente: function(){
-            this.indexImg -= 1;
-            if(this.indexImg == -1){
-                this.indexImg = this.images.length -1 ;
-            }
+        onDotClick(clickedIndex) {
+            this.indexImg = clickedIndex;
         },
-        dotOnClick:function(){
-            this.dot="color-blue dot"
-            this.indexImg = 0
-            
-           
+        play() {
+            clearInterval(this.interval);
+
+            this.interval = setInterval(() => {
+                this.changeImg(+1, true);
+            }, 5000);
         },
-        dotOnClickOne:function(){
-            this.indexImg = 1
-        },
-        dotOnClickTwo:function(){
-            this.indexImg = 2
-        },
-        dotOnClickThree:function(){
-            this.indexImg = 3
+        pause() {
+            clearInterval(this.interval);
         }
+    },
+    mounted() {
+        const elementoHtml = document.querySelector(".slider-container");
+
+        elementoHtml.focus();
+
+        this.play();
     }
+    
 
 
 });
